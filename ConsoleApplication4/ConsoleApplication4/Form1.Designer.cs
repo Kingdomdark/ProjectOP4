81c2a1b1aca02468122919b27bea55cc1d829826
rickymisier <rickymisier@hotmail.com>
2017-06-26 21:07:50 +0200

    hier is basic forms

---
+++ ConsoleApplication4/ConsoleApplication4/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PBchar = new System.Windows.Forms.PictureBox();
+            this.PbBlock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBchar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbBlock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PBchar
+            // 
+            this.PBchar.Image = ((System.Drawing.Image)(resources.GetObject("PBchar.Image")));
+            this.PBchar.Location = new System.Drawing.Point(490, 404);
+            this.PBchar.Name = "PBchar";
+            this.PBchar.Size = new System.Drawing.Size(100, 100);
+            this.PBchar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBchar.TabIndex = 0;
+            this.PBchar.TabStop = false;
+            // 
+            // PbBlock
+            // 
+            this.PbBlock.Image = ((System.Drawing.Image)(resources.GetObject("PbBlock.Image")));
+            this.PbBlock.Location = new System.Drawing.Point(490, 1);
+            this.PbBlock.Name = "PbBlock";
+            this.PbBlock.Size = new System.Drawing.Size(50, 50);
+            this.PbBlock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbBlock.TabIndex = 1;
+            this.PbBlock.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1137, 531);
+            this.Controls.Add(this.PbBlock);
+            this.Controls.Add(this.PBchar);
+            this.Name = "Form1";
+            this.Text = "Block Catcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBchar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbBlock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PBchar;
+        private System.Windows.Forms.PictureBox PbBlock;
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication4
+{
+  
+
+    public partial class Form1 : Form
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //splashbox - purpose of the game
+            MessageBox.Show("VANG ALLE BLOKJES BEHALVE DE RODE EN MIS JE 3 DAN BEN JE DOOD", "About Block Catcher");
+        }
+    }
+}
